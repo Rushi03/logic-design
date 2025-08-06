74b75fcf6ea1b0a4056d0d42983fbf101b75d8c5
Rushi03 <rushi.m.patel92@gmail.com>
2025-08-05 22:06:09 -0700

    changed the data width to be variable in design and tb

--- projects/alu/alu.sv
+++ projects/alu/alu.sv
@@ -1,14 +1,15 @@
-module ALU(input logic [31:0] a,
-           input logic [31:0] b,
+module ALU #(parameter WIDTH = 32)                                  // Width can be 8, 16, 32
+          (input logic [WIDTH-1:0] a,
+           input logic [WIDTH-1:0] b,
            input logic [3:0] opcode,
-           output logic [31:0] result,
+           output logic [WIDTH-1:0] result,
            output logic zero,
            output logic negative,
            output logic carryout,
            output logic overflow);
 
-logic signed [31:0] sA, sB;
-logic [32:0] fullAddSub; // carry bit detection
+logic signed [WIDTH-1:0] sA, sB;
+logic [WIDTH:0] fullAddSub; // carry bit detection
 
 always_comb 
     begin
@@ -16,7 +17,7 @@ always_comb
         sB = b;
         carryout = 0;
         overflow = 0;
-        fullAddSub = 33'd0;
+        fullAddSub = 0;
 
         case(opcode)
             4'b0000: result = a & b;                                // AND
@@ -27,29 +28,29 @@ always_comb
             4'b0101: result = ~a;                                   // NOT
             4'b0110: 
                 begin                                               // ADD
-                    fullAddSub = {1'b0, a} + {1'b0, a};
-                    result = fullAddSub[31:0];
-                    carryout = fullAddSub[32];
-                    overflow = (a[31] == b[31]) && (result[31] != a[31]);
+                    fullAddSub = {1'b0, a} + {1'b0, b};
+                    result = fullAddSub[WIDTH-1:0];
+                    carryout = fullAddSub[WIDTH];
+                    overflow = (a[WIDTH-1] == b[WIDTH-1]) && (result[WIDTH-1] != a[WIDTH-1]);
                 end
             4'b0111: 
                 begin                                               // SUB
                     fullAddSub = {1'b0, a} - {1'b0, b};
-                    result = fullAddSub[31:0];
-                    carryout = fullAddSub[32];
-                    overflow = (a[31] != b[31]) && (result[31] != a[31]);
+                    result = fullAddSub[WIDTH-1:0];
+                    carryout = fullAddSub[WIDTH];
+                    overflow = (a[WIDTH-1] != b[WIDTH-1]) && (result[WIDTH-1] != a[WIDTH-1]);
                 end
-            4'b1000: result = (sA < sB) ? 32'd1 : 32'd0;            // SLT - set less than
+            4'b1000: result = (sA < sB) ? 1 : 0;                    // SLT - set less than
             4'b1001: result = sA * sB;                              // MULT
-            4'b1010: result = (b != 0) ? sA / sB : 32'd0;           // DIV
-            4'b1011: result = (b != 0) ? sA % sB : 32'd0;           // MOD
+            4'b1010: result = (b != 0) ? sA / sB : 0;               // DIV
+            4'b1011: result = (b != 0) ? sA % sB : 0;               // MOD
             4'b1100: result = sA <<< 4;                             // SLA - shift left arithmetic
             4'b1101: result = sA >>> 4;                             // SRA - shift right arithmetic
-            default: result = 32'd0;
+            default: result = 0;
         endcase
 
-        zero = (result == 32'd0);
-        negative = result[31];
+        zero = (result == 0);
+        negative = result[WIDTH-1];
     end
 
 endmodule
